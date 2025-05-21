969fc7339fc7decf85514b4ecf4736b7d2b592c2
Auraria <137568419+Franky1764@users.noreply.github.com>
2025-05-21 12:38:55 -0400

    Disabled checkout button if cart is empty + DOM validation + add some products

--- scripts/cargar_productos.py
+++ scripts/cargar_productos.py
@@ -6,26 +6,47 @@ from models.producto import Producto
 Base.metadata.create_all(engine)
 session = Session()
 
-producto1 = Producto(
-    codigo="FER123",
-    marca="Truper",
-    nombre="Taladro percutor 750W",
-    modelo="TP-750",
-    stock=10,
-    precio=45990.0
-)
+# Elimina productos anteriores (si quieres partir de cero)
+session.query(Producto).delete()
 
-producto2 = Producto(
-    codigo="FER456",
-    marca="Bosch",
-    nombre="Sierra circular 1400W",
-    modelo="SC-1400",
-    stock=5,
-    precio=78990.0
-)
+# Nuevos productos
+productos = [
+    Producto(
+        codigo="FER123",
+        marca="Truper",
+        nombre="Taladro percutor 750W",
+        modelo="TP-750",
+        stock=10,
+        precio=45990.0
+    ),
+    Producto(
+        codigo="FER456",
+        marca="Bosch",
+        nombre="Sierra circular 1400W",
+        modelo="SC-1400",
+        stock=5,
+        precio=78990.0
+    ),
+    Producto(
+        codigo="FER789",
+        marca="Makita",
+        nombre="Lijadora Orbital 300W",
+        modelo="LO-300",
+        stock=7,
+        precio=64990.0
+    ),
+    Producto(
+        codigo="FER321",
+        marca="Stanley",
+        nombre="Caja de herramientas 19'' con organizador",
+        modelo="CT-19",
+        stock=12,
+        precio=19990.0
+    )
+]
 
-session.add_all([producto1, producto2])
+session.add_all(productos)
 session.commit()
 session.close()
 
-print("✔️ Productos de prueba insertados.")
+print("productos cargados correctamente.")
